a7147d96031d4d6b5dac9f15d4b08ed477ec1fd3
Quang Ly <quangdly@gmail.com>
2015-06-19 14:36:13 -0400

    Test is_payday()

--- paydays.py
+++ paydays.py
@@ -3,24 +3,50 @@ Author: Quang Ly
 """
 import decimal
 import datetime
-from datetime import timedelta
+from datetime import timedelta, date
+
+""" {'empid' : 1, 'paydate': '2015-01-15'},
+    {'empid' : 1, 'paydate': '2015-01-30'},
+"""   
 
 """mock data"""
-paycycles = [
+PAYCYCLES = [
     {'empid' : 1, 'paydate': '2015-01-01'},
-    {'empid' : 1, 'paydate': '2015-01-15'},
     {'empid' : 2, 'paydate': '2015-02-17'}
 ]
 
+FREQUENCY = 15
 
 def get_last_pay(empid):
     """get last pay
     :params: int
     :return: date
     """
-    seq = [x['paydate'] for x in paycycles if x['empid'] == empid]
+    seq = [x['paydate'] for x in PAYCYCLES if x['empid'] == empid]
     return(datetime.datetime.strptime(max(seq), "%Y-%m-%d").date())
     
+
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+def dayrange(start_date, end_date):
+    return (end_date - start_date).days
+
+def is_payday(empid, date = None):
+    """for given empid and date, return bool on valid paydate
+    :params: int, date
+    :return bool
+    """
+    'get last paydate'
+    if date is None:
+        date = datetime.datetime.now().date()
+    last_paydate = get_last_pay(empid)
+
+    'return bool whether range is divisible by FREQUENCY paycycle'
+    'TOOO: add exception to holidays, etc'
+    return dayrange(last_paydate, date) % FREQUENCY == 0
+
 
 def move_days(input_date, days):
     """
@@ -29,6 +55,6 @@ def move_days(input_date, days):
     assert(type(input_date) is datetime.date)
     assert(type(days) is int)
     newdate = input_date + timedelta(days = days)
-    message = "Input date {inputdate}: \t Days {days} \t New date {newdate}".format(inputdate=input_date, days=days, newdate=newdate)
-    print(message)
+    # message = "Input date {inputdate}: \t Days {days} \t New date {newdate}".format(inputdate=input_date, days=days, newdate=newdate)
+    # print(message)
     return newdate
--- tests/test_paydays.py
+++ tests/test_paydays.py
@@ -21,15 +21,36 @@ class TestHelper(unittest.TestCase):
         """For given empid, get latest pay"""
         empid = 1
         last_pay_date = paydays.get_last_pay(1)
-        check_date = date(2015, 1, 15)
+        check_date = date(2015, 1, 1)
         assert(check_date == last_pay_date)
 
-    def test_is_payday_date(self):
-        """whether a given day is a payday for that user. 
-        If a date is not specified, default to today's date. 
-        Return a boolean value"""
-        d = date(2015, 1, 30)
+    def test_dayrange(self):
+        "test get number of days bewteen date"
+        start_date = date(2015, 1, 1)
+        end_date = date(2015, 1, 31)
+        # print(30 % 15)
+        print(paydays.dayrange(start_date, end_date))
+
+
+    def test_is_payday_true(self):
+        """Whether a given day IS a payday for that user. If a date is not specified, default to today's date. """
+        d = date(2015, 1, 31)
         empid = 1
+        is_paydate = paydays.is_payday(empid, d)
+        assert(is_paydate == True)
+
+    def test_is_payday_true_false(self):
+        """Whether a given day is NOT payday for that user. If a date is not specified, default to today's date. """
+        d = date(2015, 1, 15)
+        empid = 1
+        is_paydate = paydays.is_payday(empid, d)
+        assert(is_paydate == False)
+
+    def test_is_payday_current_day(self):
+        "If no payday given, use current day"
+        empid = 1
+        is_paydate = paydays.is_payday(empid, None)
+        assert(is_paydate == False)
 
     def test_move_days_plus(self):
         "Test add 10 move_days"
